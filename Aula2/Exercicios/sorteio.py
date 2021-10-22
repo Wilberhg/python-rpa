a4dbcc5b06142408853accefbb11513cab2450ef
Wilberhg <37167412+Wilberhg@users.noreply.github.com>
2021-10-21 21:08:18 -0300

    Delete Aula2/Exercicios directory

--- Aula2/Exercicios/sorteio.py
+++
@@ -1,9 +0,0 @@
-import random
-lista_de_alunos = []
-while len(lista_de_alunos) < 5:
-    tamanho_lista = len(lista_de_alunos)
-    nome = input(f'Por favor insira o nome do aluno de número {tamanho_lista+1}: ')
-    lista_de_alunos.append(nome)
-print(f'\nA ordenação era: {lista_de_alunos}')
-random.shuffle(lista_de_alunos)
-print(f'\nA ordenação ficou assim: {lista_de_alunos}')
